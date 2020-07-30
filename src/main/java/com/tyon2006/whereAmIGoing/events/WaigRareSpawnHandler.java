d3a475d8bf0fb6e4b1a2a0109bda8bda13f38846
tyon2006 <nicholas.tyo@gmail.com>
2020-07-29 21:18:29 -0400

    fixed recursive NBT call
    thanks Kindlich!

--- src/main/java/com/tyon2006/whereAmIGoing/events/WaigRareSpawnHandler.java
+++ src/main/java/com/tyon2006/whereAmIGoing/events/WaigRareSpawnHandler.java
@@ -89,81 +89,75 @@ public class WaigRareSpawnHandler {
 	//@SideOnly (Side.SERVER)
 	//public void onMobJoinDoRarespawn(EntityJoinWorldEvent event) {	
 	public void onMobJoinDoRarespawn(LivingSpawnEvent.CheckSpawn event) {	
+		
         if (event.getWorld().isRemote) return;
+		if (!(event.getEntity() instanceof EntityLiving)) return;
+        if (event.getEntityLiving().isDead) return;
         
-		if (event.getEntity() instanceof EntityLiving) {	
-			
-			Entity entity = event.getEntity();
-			EntityLiving entityLiving = (EntityLiving) entity;
-			NBTTagCompound entityNBT = entity.getEntityData();
-			NBTTagCompound healthtag = new NBTTagCompound();
-			
-			if (entityNBT.hasKey("waigRareSpawnChecked")){
-				System.out.println("this muthafucka already bonused");
-				return;
-			} 
-			if (ConfigManager.rareSpawnMap.containsKey(entity.getName().toLowerCase()))
-			{
-				//GlStateManager.generateTexture()color(10, 10, 10);
-				healthtag.setBoolean("waighealth", true); //tag the mob so it doesn't get modified later
-				
-				
-				System.out.println("MAP OUTPUTFOR " + entity.getName().toLowerCase());
-				System.out.println(ConfigManager.rareSpawnMap.get(entity.getName().toLowerCase())); 
-				//BossInfoServer bossInfo = new BossInfoServer(entity.getDisplayName(), BossInfo.Color.GREEN, BossInfo.Overlay.NOTCHED_10);
-				mobAttMap.putAll(ConfigManager.rareSpawnMap.get(entity.getName().toLowerCase()));
-				System.out.println("FOUND A THING NAMED " + entity.getName().toLowerCase());
-				System.out.println(ConfigManager.rareSpawnMap.toString()); 
-				System.out.println(ConfigManager.rareSpawnMap.get(entity.getName().toLowerCase())); 
-				System.out.println(ConfigManager.rareSpawnMap.get(entity.getName().toLowerCase()).get("spawnchance"));
-				System.out.println(mobAttMap.toString()); 
-				System.out.println(mobAttMap.get("spawnchance")); 
-				
-				System.out.println("adding health to: " + entity.getName() + " " + event.getEntity().getEntityId());
-				IAttributeInstance entityHealth = entityLiving.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH);
-				Random rand = new Random();
-				int randy = rand.nextInt(100);
-				double maxHealth = 0;
-				entityLiving.writeToNBT(healthtag);	//i think this is bad //no, it isnt
-				/*
-				maxHealth = entityHealth.getBaseValue() + 10;
-				
-				//entityLiving.writeToNBT(healthtag);	//i think this is bad
-				//entityLiving.setHealth((float) maxHealth); //remove this see what happens
-				
-				entityHealth.setBaseValue(maxHealth); //is this what actually increases the health?
-				entityLiving.setCustomNameTag("HEALTH:" + String.valueOf(maxHealth)+ " ID:" + event.getEntity().getEntityId() );
-				*/
-				
-				if (randy > 5) {
-		            EntityLivingBase mob = entityLiving;
-					System.out.println("WINNER WINNER " + entity.getClass() + " " + entity.getEntityId() + " DINNER. RAND - " + randy);
-					maxHealth = entityHealth.getBaseValue() + 10;
-					entityHealth.setBaseValue(maxHealth);//is this what actually increases the health?
-					entityLiving.setHealth((float)maxHealth);
-					
-					
-					
-					healthtag.setBoolean("waigIsRare", true);
-					//entityLiving.writeToNBT(healthtag);
-					//entity.writeToNBT(healthtag);
-					//entityNBT.setBoolean("waigIsRare", true);
-					entityNBT.setTag("waigIsRare", healthtag);
-					
-					entityLiving.setCustomNameTag(ConfigManager.rareSpawnMap.get(entity.getName().toLowerCase()).get("spawnname"));// + "HEALTH:" + " ID:" + event.getEntity().getEntityId() );
-					entity.setAlwaysRenderNameTag(true);
-		            mob.setGlowing(true);
-		            //System.out.println("WINNER WINNER " + entity.readFromNBT();
-		            
-		            mob.addPotionEffect(new PotionEffect(MobEffects.REGENERATION, 1000));
-		            //addPotionEffect(new EffectInstance(Effects.INVISIBILITY, 200));
-		            randy = 0;
-		            mobAttMap.clear();
-				}
-			}
+		Entity entity = event.getEntity();
+		EntityLiving entityLiving = (EntityLiving) entity;
+		NBTTagCompound entityNBT = entity.getEntityData();
+		NBTTagCompound healthtag = new NBTTagCompound();
+		
+		if (entityNBT.hasKey("waigRareSpawnChecked")){
+			System.out.println("this muthafucka already bonused");
+			return;
 		} 
-		else {
-				return;
+		if (ConfigManager.rareSpawnMap.containsKey(entity.getName().toLowerCase()))
+		{
+			//GlStateManager.generateTexture()color(10, 10, 10);
+			healthtag.setBoolean("waighealth", true); //tag the mob so it doesn't get modified later
+			
+			System.out.println("MAP OUTPUTFOR " + entity.getName().toLowerCase());
+			System.out.println(ConfigManager.rareSpawnMap.get(entity.getName().toLowerCase())); 
+			//BossInfoServer bossInfo = new BossInfoServer(entity.getDisplayName(), BossInfo.Color.GREEN, BossInfo.Overlay.NOTCHED_10);
+			mobAttMap.putAll(ConfigManager.rareSpawnMap.get(entity.getName().toLowerCase()));
+			System.out.println("FOUND A THING NAMED " + entity.getName().toLowerCase());
+			System.out.println(ConfigManager.rareSpawnMap.toString()); 
+			System.out.println(ConfigManager.rareSpawnMap.get(entity.getName().toLowerCase())); 
+			System.out.println(ConfigManager.rareSpawnMap.get(entity.getName().toLowerCase()).get("spawnchance"));
+			System.out.println(mobAttMap.toString()); 
+			System.out.println(mobAttMap.get("spawnchance")); 
+			
+			System.out.println("adding health to: " + entity.getName() + " " + event.getEntity().getEntityId());
+			IAttributeInstance entityHealth = entityLiving.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH);
+			Random rand = new Random();
+			int randy = rand.nextInt(100);
+			double maxHealth = 0;
+			entityLiving.writeToNBT(healthtag);	//i think this is bad //no, it isnt
+			/*
+			maxHealth = entityHealth.getBaseValue() + 10;
+			
+			//entityLiving.writeToNBT(healthtag);	//i think this is bad
+			//entityLiving.setHealth((float) maxHealth); //remove this see what happens
+			
+			entityHealth.setBaseValue(maxHealth); //is this what actually increases the health?
+			entityLiving.setCustomNameTag("HEALTH:" + String.valueOf(maxHealth)+ " ID:" + event.getEntity().getEntityId() );
+			*/
+			
+			if (randy > 5) {
+	            EntityLivingBase mob = entityLiving;
+				System.out.println("WINNER WINNER " + entity.getClass() + " " + entity.getEntityId() + " DINNER. RAND - " + randy);
+				maxHealth = entityHealth.getBaseValue() + 10;
+				entityHealth.setBaseValue(maxHealth);//is this what actually increases the health?
+				entityLiving.setHealth((float)maxHealth);
+				
+				//healthtag.setBoolean("waigIsRare", true);
+				//entityNBT.setTag("waigIsRare", healthtag);
+				
+				entityNBT.setBoolean("waigIsRare", true);
+				
+				entityLiving.setCustomNameTag(ConfigManager.rareSpawnMap.get(entity.getName().toLowerCase()).get("spawnname"));// + "HEALTH:" + " ID:" + event.getEntity().getEntityId() );
+				entity.setAlwaysRenderNameTag(true);
+	            mob.setGlowing(true);
+	            //System.out.println("WINNER WINNER " + entity.readFromNBT();
+	            
+	            mob.addPotionEffect(new PotionEffect(MobEffects.REGENERATION, 1000));
+	            //addPotionEffect(new EffectInstance(Effects.INVISIBILITY, 200));
+	            randy = 0;
+	            //NBTTagCompound.writeEntry(null, entityNBT, null);
+	            mobAttMap.clear();
+				}
 			}
 		}
 	
