a79e82005351b23ba92d77ed80ce4a88513b4e4e
tyon2006 <nicholas.tyo@gmail.com>
2022-08-07 20:04:42 -0400

    minor update

--- src/main/java/com/tyon2006/whereAmIGoing/util/Reference.java
+++ src/main/java/com/tyon2006/whereAmIGoing/util/Reference.java
@@ -9,7 +9,7 @@ public class Reference {
 	
 	public static final String AUTHOR = "tyon2006";
 
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "1.4.4";
 
 	public static final String ACCEPTED_VERSIONS = "[1.12.2]";
 	
